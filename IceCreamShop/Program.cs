283744c185738c185448fb811fad19ec669f562b
Dan Cowdry <medtc411@gmail.com>
2021-12-03 07:31:36 -0800

    finished up to bonus

--- IceCreamShop/Program.cs
+++ IceCreamShop/Program.cs
@@ -11,13 +11,38 @@ namespace IceCreamShop
             List<Flavor> availableFlavors = menu.Flavors;
             List<Cone> availableCones = menu.Cones;
 
-        // TODO: Use a Comparer class to sort the 'flavors' array alphabetically by the 'name'
-        //  field.
 
-        // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost'
-        //  field.
 
-        // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
+            // TODO: Use a Comparer class to sort the 'flavors' array alphabetically by the 'name'
+            //  field.
+            FlavorComparer comparer = new FlavorComparer();
+            Console.WriteLine("------------Before comparing---------------");
+            foreach (Flavor flavor in availableFlavors)
+            {
+                Console.WriteLine(flavor.Name);
+            }
+            availableFlavors.Sort(comparer);
+            Console.WriteLine("------------After comparing---------------");
+            foreach (Flavor flavor in availableFlavors)
+            {
+                Console.WriteLine(flavor.Name);
+            }
+
+            // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost'
+            //  field.
+            ConeComparer comparer2 = new ConeComparer();
+            Console.WriteLine("------------Before comparing---------------");
+            foreach (Cone cone in availableCones)
+            {
+                Console.WriteLine($"{cone.Name}: ${cone.Cost}");
+            }
+            availableCones.Sort(comparer2);
+            Console.WriteLine("------------After comparing---------------");
+            foreach (Cone cone in availableCones)
+            {
+                Console.WriteLine($"{cone.Name}: ${cone.Cost}");
+            }
+            // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
         }
     }
 }
